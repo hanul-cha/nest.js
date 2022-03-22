39bfbb198f69e9460ffc072ba0908c7bccd503be
차한울 <caroneul@datable.co.kr>
2022-03-22 23:19:43 +0900

    비밀번호 암호화

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,5 +1,30 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcypt from 'bcryptjs';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(AuthCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = AuthCredentialDto;
+
+    const salt = await bcypt.genSalt();
+    const hashedPassword = await bcypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
