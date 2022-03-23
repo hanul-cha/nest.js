280b5b581568b354effa6d8a92ff02373c50a04e
차한울 <caroneul@datable.co.kr>
2022-03-23 23:10:05 +0900

    jwt 토큰 생성하기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,12 +4,19 @@ import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private userService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
   @Post('/signup')
   signUp(
     @Body(ValidationPipe) authCredentialDto: AuthCredentialDto,
   ): Promise<void> {
-    return this.userService.signUp(authCredentialDto);
+    return this.authService.signUp(authCredentialDto);
+  }
+
+  @Post('/signin')
+  signIn(
+    @Body(ValidationPipe) AuthCredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(AuthCredentialDto);
   }
 }
--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -5,15 +5,15 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
-import * as bcypt from 'bcryptjs';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(AuthCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = AuthCredentialDto;
 
-    const salt = await bcypt.genSalt();
-    const hashedPassword = await bcypt.hash(password, salt);
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const user = this.create({ username, password: hashedPassword });
 
